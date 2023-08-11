81d451648c1669a7fbd21bbd38824b3ab32bed72
emmanuelwithme <wow90327@gmail.com>
2023-08-12 02:23:21 +0800

    add ForChin & change MollyInMexico to ForRuth

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from dotenv import load_dotenv
 import os
 import spotipy
@@ -18,26 +18,44 @@ sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID, 
 
 @app1.route('/')
 def index():
+    return render_template('index.html')
+
+@app1.route('/ForRuth')
+def ForRuth():
     # 獲取Spotify歌曲的URL
     track_url = get_spotify_track_url('Lauv - Molly In Mexico')
-    return render_template('MollyInMexico.html', track_url=track_url)
+    return render_template('ForRuth.html', track_url=track_url)
+
+@app1.route('/ForChin')
+def ForTiffany():
+    return render_template('ForChin.html')
 
 def get_spotify_track_url(track_name):
     results = sp.search(q=track_name, limit=1)
     track = results['tracks']['items'][0]
     return track['external_urls']['spotify']
 
-# 定義一個新路由，用於從content.txt文件中獲取內容
+# 定義一個新路由，用於從letter/*.txt文件中獲取內容
 @app1.route('/get-content', methods=['GET'])
 def get_content():
+    # 從GET請求的查詢參數中獲取文件路徑。
+    # 例如，當前端發送請求到 /get-content?file_path=letter/content.txt，則 file_path 將會是 "letter/content.txt"
+    file_path = request.args.get('file_path')
+    
+    # 安全檢查：確保請求的文件路徑是在 'letter/' 目錄下。
+    # 這是為了防止使用者嘗試訪問其他不應被許可的目錄或文件。
+    if not file_path.startswith('letter/'):
+        return jsonify({'error': 'Invalid file path'}), 400
+
     try:
-        # 使用with語句打開文件，這樣可以確保文件在讀取後被正確關閉
-        with open(os.path.join('letter', 'content.txt'), 'r', encoding='utf-8') as file:
+        # 使用with語句確保文件在操作後被正確關閉。
+        with open(file_path, 'r', encoding='utf-8') as file:
+            # 讀取文件內容
             content = file.read()
         # 返回文件內容作為JSON響應
         return jsonify({'content': content}), 200
     except Exception as e:
-        # 如果出現任何錯誤，返回錯誤消息
+        # 如果讀取文件過程中出現錯誤，返回錯誤信息
         return jsonify({'error': str(e)}), 500
 
 # 啟動Flask應用程序
