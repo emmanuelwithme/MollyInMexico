c119d57eff31c8d6f986b3d47735c05a9b428acb
emmanuelwithme <wow90327@gmail.com>
2023-08-11 08:23:19 +0800

    spotify + youtube Lauv - Molly In Mexico 背景播放(都沒成功)

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from flask import Flask, jsonify, render_template
+from dotenv import load_dotenv
 import os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 app1 = Flask(__name__)
 
-# 定義根路由，這將渲染HTML模板
+# 加載.env文件
+load_dotenv()
+
+# 從環境變量中獲取 Spotify 客戶端ID和密鑰
+CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
+CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
+
+# 設置Spotify API認證
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET))
+
 @app1.route('/')
 def index():
-    return render_template('MollyInMexico.html')
+    # 獲取Spotify歌曲的URL
+    track_url = get_spotify_track_url('Lauv - Molly In Mexico')
+    return render_template('MollyInMexico.html', track_url=track_url)
+
+def get_spotify_track_url(track_name):
+    results = sp.search(q=track_name, limit=1)
+    track = results['tracks']['items'][0]
+    return track['external_urls']['spotify']
 
 # 定義一個新路由，用於從content.txt文件中獲取內容
 @app1.route('/get-content', methods=['GET'])
